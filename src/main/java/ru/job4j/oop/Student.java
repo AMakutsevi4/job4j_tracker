c850e7ab4dbe0d822590f9c4576a1b5a34ab3c92
AMakutsevi4 <Qwerty123>
2021-05-06 14:05:56 +0900

    1.2. Вызов метода объекта [#487839]

---
+++ src/main/java/ru/job4j/oop/Student.java
@@ -0,0 +1,21 @@
+package ru.job4j.oop;
+
+public class Student {
+    public void music() {
+        System.out.println("Tra li vally ");
+    }
+
+    public void voice() {
+        System.out.println("I believe I can fly");
+    }
+
+    public static void main(String[] args) {
+        Student petya = new Student();
+        petya.music();
+        petya.music();
+        petya.music();
+        petya.voice();
+        petya.voice();
+        petya.voice();
+    }
+}
