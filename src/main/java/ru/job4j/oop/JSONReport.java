504d1b4993aabd4fe9a33caa2bdfd4f2841bc285
AMakutsevi4 <Qwerty123>
2021-05-15 14:34:48 +0900

    4. Переопределение [#487850]

---
+++ src/main/java/ru/job4j/oop/JSONReport.java
@@ -0,0 +1,13 @@
+package ru.job4j.oop;
+
+public class JSONReport extends TextReport {
+    public String generate(String name, String body) {
+        return "name " + name + System.lineSeparator() + "body " + body;
+    }
+
+    public static void main(String[] args) {
+        JSONReport report = new JSONReport();
+        String text = report.generate("Report's name", "Report's body");
+        System.out.println(text);
+    }
+}
