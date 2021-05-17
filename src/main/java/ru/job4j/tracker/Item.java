c9ebf61766538c21dc47b9de78abc3a9c79b20fd
AMakutsevi4 <Qwerty123>
2021-05-17 14:38:42 +0900

    7. Date. Отображение даты. [#487853]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,8 +1,12 @@
 package ru.job4j.tracker;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Item {
     private int id;
     private String name;
+    private LocalDateTime created = LocalDateTime.now();
 
     public Item(int id) {
         this.id = id;
@@ -31,5 +35,11 @@ public class Item {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getCreated() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
+        String timeFormat = created.format(formatter);
+        return timeFormat;
     }
 }
---
+++ src/main/java/ru/job4j/tracker/StartUl.java
@@ -0,0 +1,9 @@
+package ru.job4j.tracker;
+
+public class StartUl extends Item {
+
+    public static void main(String[] args) {
+        StartUl item = new StartUl();
+        System.out.println(item.getCreated());
+    }
+}
