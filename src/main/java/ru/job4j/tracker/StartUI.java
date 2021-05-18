6f1fbe92353d871fea7228f1d2a91c1e6462f859
AMakutsevi4 <Qwerty123>
2021-05-18 17:29:11 +0900

    5. Tracker - хранилище [#487866]

---
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -0,0 +1,12 @@
+package ru.job4j.tracker;
+
+public class StartUI {
+    public static void main(String[] args) {
+        Tracker tracker = new Tracker();
+        Item item1 = new Item("vasya");
+        Item item2 = new Item("petya");
+        tracker.add(item1);
+        tracker.add(item2);
+        System.out.println(tracker.findById(1));
+    }
+}
