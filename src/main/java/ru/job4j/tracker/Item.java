a04f1c568e86f9d85659f4fecd277183f248d019
AMakutsevi4 <Qwerty123>
2021-05-20 15:40:56 +0900

    9. toString [#310018 #158426]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -7,7 +7,22 @@ public class Item {
     private int id;
 
     private String name;
+
     private LocalDateTime created = LocalDateTime.now();
+
+    public Item(int id) {
+
+        this.id = id;
+    }
+
+    public Item() {
+
+    }
+
+    public Item(String name) {
+
+        this.name = name;
+    }
 
     @Override
     public String toString() {
@@ -18,32 +33,23 @@ public class Item {
                 + '}';
     }
 
-    public Item(int id) {
-        this.id = id;
-    }
-
-    public Item() {
-
-    }
-
-    public Item(String name) {
-        this.name = name;
-    }
-
     public int getId() {
 
         return id;
     }
 
     public void setId(int id) {
+
         this.id = id;
     }
 
     public String getName() {
+
         return name;
     }
 
     public void setName(String name) {
+
         this.name = name;
     }
 
