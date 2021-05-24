2f79dab52500367b63371b9723800b46981a9b98
AMakutsevi4 <Qwerty123>
2021-05-24 14:12:33 +0900

    2.1. Реализация класса StartUI [#784]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -1,13 +1,91 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class StartUI {
+
+    private final Scanner console = new Scanner(System.in);
+
+    public void init(Scanner scanner, Tracker tracker) {
+        boolean run = true;
+        while (run) {
+            this.showMenu();
+            System.out.print("Выберите: ");
+            int select = Integer.valueOf(scanner.nextLine());
+            if (select == 0) {
+                System.out.println("=== Создайте новый предмет ====");
+                System.out.print("Введите имя: ");
+                String name = console.nextLine();
+                Item item = new Item(name);
+                tracker.add(item);
+            } else if (select == 1) {
+                System.out.println("=== Показать все предметы: ====");
+                Item[] items = tracker.findAll();
+                for (Item i : items) {
+                    System.out.println(i);
+                }
+            } else if (select == 2) {
+                System.out.println("=== Редактировать элемент: ====");
+                System.out.println("Введите id заявки");
+                int id = Integer.valueOf(scanner.nextLine());
+                System.out.println("Введите новое имя элемента");
+                String name = console.nextLine();
+                Item item = new Item(name);
+                tracker.replace(id, item);
+                if (tracker.replace(id, item)) {
+                    System.out.println("Элемент изменен");
+                } else {
+                    System.out.println("Ошибка");
+                }
+            } else if (select == 3) {
+                System.out.println("Введите id заявки");
+                int id = console.nextInt();
+                if (tracker.delete(id)) {
+                    System.out.println("Элемент удален");
+                } else {
+                    System.out.println("Ошибка");
+                }
+
+            } else if (select == 4) {
+                System.out.println("Введите id заявки");
+                int id = console.nextInt();
+                Item item = tracker.findById(id);
+                boolean rsl = item != null;
+                if (rsl) {
+                    System.out.println("Элемент найден: " + item);
+                } else {
+                    System.out.println("Заявка с таким id не найдена");
+                }
+            } else if (select == 5) {
+                System.out.println("Введите имя заявки");
+                String name = console.nextLine();
+                Item[] item = tracker.findByName(name);
+                if (item.length > 0) {
+                    System.out.println("Элемент найден: " + Arrays.toString(item));
+                } else {
+                    System.out.println("Заявки с таким именем не найдены");
+                }
+            } else if (select == 6) {
+                run = false;
+            }
+        }
+    }
+
+    private void showMenu() {
+        System.out.println("Меню.");
+        System.out.println("0. Добавить новый элемент\n"
+                + "1. Показать все предметы\n"
+                + "2. Редактировать элемент\n"
+                + "3. Удалить элемент'\n"
+                + "4. Найти объект по идентификатору (id)\n"
+                + "5. Найти объект по названию\n"
+                + "6. Выйти из программы");
+    }
+
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         Tracker tracker = new Tracker();
-        Item item1 = new Item("vasya");
-        Item item2 = new Item("petya");
-        tracker.add(item1);
-        tracker.add(item2);
-        System.out.println(tracker.findById(1));
-        System.out.println(item2);
+        new StartUI().init(scanner, tracker);
     }
 }
