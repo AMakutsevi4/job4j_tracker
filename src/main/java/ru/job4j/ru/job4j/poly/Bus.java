f2430db7e431d09d7724c055e5f9392f192e2fc8
AMakutsevi4 <Qwerty123>
2021-05-25 17:19:58 +0900

    3. Интерфейс Input [#181092]

---
+++ src/main/java/ru/job4j/ru/job4j/poly/Bus.java
@@ -0,0 +1,18 @@
+package ru.job4j.ru.job4j.poly;
+
+public class Bus implements Transport {
+    @Override
+    public int gofar() {
+        return 0;
+    }
+
+    @Override
+    public int sayInt(int question) {
+        return 0;
+    }
+
+    @Override
+    public int fuelInt(String question) {
+        return 0;
+    }
+}
---
+++ src/main/java/ru/job4j/ru/job4j/poly/Transport.java
@@ -0,0 +1,10 @@
+package ru.job4j.ru.job4j.poly;
+
+public interface Transport {
+
+    int gofar();
+
+    int sayInt(int question);
+
+    int fuelInt(String question);
+}
