73c3553531e3c9514c521670c72ca26e0b963a02
AMakutsevi4 <Qwerty123>
2021-05-25 14:38:00 +0900

    2.2. Реализация класса StartUI. Вывод меню [#500743]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -1,85 +1,34 @@
 package ru.job4j.tracker;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class StartUI {
 
-      public void init(Scanner scanner, Tracker tracker) {
+    public void init(Scanner scanner, Tracker tracker) {
+        String[] menu = {
+                "Add new Item", "Show all items", "Edit item",
+                "Delete item", "Find item by id", "Find items by name",
+                "Exit Program"
+        };
+        System.out.println("Menu:");
+        for (int i = 0; i < menu.length; i++) {
+            System.out.println(i + ". " + menu[i]);
+        }
         boolean run = true;
         while (run) {
-            this.showMenu();
-            System.out.print("Выберите: ");
-            int select = Integer.valueOf(scanner.nextLine());
-            if (select == 0) {
-                System.out.println("=== Создайте новый предмет ====");
-                System.out.print("Введите имя: ");
-                String name = scanner.nextLine();
-                Item item = new Item(name);
-                tracker.add(item);
-            } else if (select == 1) {
-                System.out.println("=== Показать все предметы: ====");
-                Item[] items = tracker.findAll();
-                for (Item i : items) {
-                    System.out.println(i);
-                }
-            } else if (select == 2) {
-                System.out.println("=== Редактировать элемент: ====");
-                System.out.println("Введите id заявки");
-                int id = Integer.valueOf(scanner.nextLine());
-                System.out.println("Введите новое имя элемента");
-                String name = scanner.nextLine();
-                Item item = new Item(name);
-                if (tracker.replace(id, item)) {
-                    System.out.println("Элемент изменен");
-                } else {
-                    System.out.println("Ошибка");
-                }
-            } else if (select == 3) {
-                System.out.println("Введите id заявки");
-                int id = Integer.valueOf(scanner.nextLine());
-                if (tracker.delete(id)) {
-                    System.out.println("Элемент удален");
-                } else {
-                    System.out.println("Ошибка");
-                }
-
-            } else if (select == 4) {
-                System.out.println("Введите id заявки");
-                int id = Integer.valueOf(scanner.nextLine());
-                Item item = tracker.findById(id);
-                boolean rsl = item != null;
-                if (rsl) {
-                    System.out.println("Элемент найден: " + item);
-                } else {
-                    System.out.println("Заявка с таким id не найдена");
-                }
-            } else if (select == 5) {
-                System.out.println("Введите имя заявки");
-                String name = scanner.nextLine();
-                Item[] item = tracker.findByName(name);
-                if (item.length > 0) {
-                    for (int i = 0; i < item.length; i++) {
-                        System.out.println("Элемент найден: " + item[i]);
-                    }
-                } else {
-                    System.out.println("Заявки с таким именем не найдены");
-                }
-            } else if (select == 6) {
+            showMenu();
+            System.out.print("Select: ");
+            int select = Integer.parseInt(scanner.nextLine());
+            if (select != 6) {
+                System.out.println("Пользователь выбрал: " + select);
+            } else {
                 run = false;
             }
         }
     }
 
     private void showMenu() {
-        System.out.println("Меню.");
-        System.out.println("0. Добавить новый элемент\n"
-                + "1. Показать все предметы\n"
-                + "2. Редактировать элемент\n"
-                + "3. Удалить элемент'\n"
-                + "4. Найти объект по идентификатору (id)\n"
-                + "5. Найти объект по названию\n"
-                + "6. Выйти из программы");
+
     }
 
     public static void main(String[] args) {
