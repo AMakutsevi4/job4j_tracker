3a59fabc4397fcfd12e6c1b27630ab540cc80af9
AMakutsevi4 <Qwerty123>
2021-05-27 14:34:24 +0900

    5.1. Тестирование. Подготовка данных. [#182960]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -17,4 +17,27 @@ public class StartUITest {
         Item expected = new Item("Fix PC");
         assertThat(created.getName(), is(expected.getName()));
     }
+
+    @Test
+    public void whenDeleteItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("new item");
+        tracker.add(item);
+        int id = item.getId();
+        item.setName("replaced item");
+        assertThat(tracker.findById(id).getName(), is("replaced item"));
+    }
+
+    @Test
+    public void whenReplaceItems() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("null");
+        tracker.add(item);
+        int id = item.getId();
+        if (tracker.delete(id)) {
+            System.out.println("Заявка удалена успешно.");
+        } else {
+            assertThat(tracker.delete(id), is("null"));
+        }
+    }
 }
