163b08fdbd36faabfdba00ab95addb7e8bd8aece
AMakutsevi4 <Qwerty123>
2021-06-01 11:12:38 +0900

    0.3. Тестирование исключений с junit [#219366]

---
+++ src/test/java/ru/job4j/ex/FactTest.java
@@ -0,0 +1,22 @@
+package ru.job4j.ex;
+
+import org.junit.Test;
+import ru.job4j.ru.job4j.ex.Fact;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class FactTest {
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenStartGreaterThenFinish() {
+        int rsl = Fact.calc(-1);
+        assertThat(rsl, is(24));
+    }
+
+    @Test
+    public void when4Then24Finish() {
+        int rsl = Fact.calc(4);
+        assertThat(rsl, is(24));
+    }
+}
