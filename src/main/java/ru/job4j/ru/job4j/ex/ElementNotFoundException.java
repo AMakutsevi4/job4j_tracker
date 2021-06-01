ce260d8aaa9aa8d17d5e0cf712ded3f32939108f
AMakutsevi4 <Qwerty123>
2021-06-01 17:19:45 +0900

    0.4. Пользовательские исключения. [#219367 #162902]

--- src/main/java/ru/job4j/ru/job4j/ex/ElementNotFoundException.java
+++ src/main/java/ru/job4j/ru/job4j/ex/ElementNotFoundException.java
@@ -3,9 +3,10 @@ package ru.job4j.ru.job4j.ex;
 public class ElementNotFoundException extends Exception {
     public static int indexOf(String[] value, String key) throws ElementNotFoundException {
         int rsl = -1;
-        for (int i = 0; i <= value.length - 1; i++) {
+        for (int i = 0; i < value.length; i++) {
             if (value[i].equals(key)) {
                 rsl = i;
+                break;
             }
         }
         if (rsl == -1) {
@@ -17,7 +18,7 @@ public class ElementNotFoundException extends Exception {
     public static void main(String[] args) {
         String[] array = {"Hello", "World", "Idea", "Java"};
         try {
-            System.out.println(indexOf(array, "number"));
+            System.out.println(indexOf(array, "Java"));
         } catch (ElementNotFoundException e) {
             System.out.println("не существует в массиве");
             e.printStackTrace();
