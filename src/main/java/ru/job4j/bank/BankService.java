6bfb465cde800ac12dc9b6a39eda4404c0d7a106
AMakutsevi4 <Qwerty123>
2021-06-28 09:01:17 +0900

    3. Банковские переводы [#10038]

--- src/main/java/ru/job4j/bank/BankService.java
+++ src/main/java/ru/job4j/bank/BankService.java
@@ -18,8 +18,10 @@ public class BankService {
     public void addAccount(String passport, Account account) {
         User user = findByPassport(passport);
         List<Account> accounts = users.get(user);
-        if (!accounts.contains(account)) {
-            accounts.add(account);
+        if (user != null) {
+            if (!accounts.contains(account)) {
+                accounts.add(account);
+            }
         }
     }
 
@@ -49,10 +51,12 @@ public class BankService {
                                  String destPassport, String destRequisite, double amount) {
         Account oneAcc = findByRequisite(srcPassport, srcRequisite);
         Account twoAcc = findByRequisite(destPassport, destRequisite);
-        if (oneAcc.getBalance() >= amount) {
-            oneAcc.setBalance(oneAcc.getBalance() - amount);
-            twoAcc.setBalance(twoAcc.getBalance() + amount);
-            return true;
+        if (oneAcc != null && twoAcc != null) {
+            if (oneAcc.getBalance() >= amount) {
+                oneAcc.setBalance(oneAcc.getBalance() - amount);
+                twoAcc.setBalance(twoAcc.getBalance() + amount);
+                return true;
+            }
         }
         return false;
     }
