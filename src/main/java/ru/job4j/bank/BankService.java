30065e78b63b2669779d4e31130d67e2f00a6859
AMakutsevi4 <Qwerty123>
2021-06-30 09:54:33 +0900

    2. Что такое JavaDoc [#313879]

--- src/main/java/ru/job4j/bank/BankService.java
+++ src/main/java/ru/job4j/bank/BankService.java
@@ -7,13 +7,30 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * Класс описывает работу модели банковской системы
+ * @author Makutsevich
+ * @version 1.0
+ */
 public class BankService {
+    /**
+     * Это поле содержит всех пользователей системы с привязанными к ним счетами.
+     */
     private Map<User, List<Account>> users = new HashMap<>();
 
+    /**
+     * Этот метод добавляет пользователя в систему
+     * Если такого пользователя еще нет, добавляет его в список.
+     */
     public void addUser(User user) {
         List<Account> accounts = new ArrayList<>();
         users.putIfAbsent(user, accounts);
     }
+    /**
+     * Метод принимает на вход паспорт и счет, добавляет счет по пасспорту.
+     * По паспорту находит пользователя с помощью метода findByPassport(passport).
+     * Добавляет новый счет пользователю, если такого счета еще нет.
+     */
 
     public void addAccount(String passport, Account account) {
         User user = findByPassport(passport);
@@ -24,6 +41,10 @@ public class BankService {
             }
         }
     }
+    /**
+     * Метод принимает на вход паспорт, позволяя по нему получить пользователя.
+     * сравнивает значение пасспорта пользователя и входное значение.
+     */
 
     public User findByPassport(String passport) {
         for (User user : users.keySet()) {
@@ -33,6 +54,13 @@ public class BankService {
         }
         return null;
     }
+    /**
+     * Метод принимает на вход паспорт и реквизит, позволяя по ним получить счет.
+     * По паспорту находит пользователя с помощью метода findByPassport(passport).
+     * Перебирает список счетов пользователя.
+     * Сравнивает значение реквизитов счета и входное значение.
+     *@return счет или null, если счета с таким реквизитом нету
+      */
 
     public Account findByRequisite(String passport, String requisite) {
         User user = findByPassport(passport);
@@ -46,6 +74,11 @@ public class BankService {
         }
         return null;
     }
+    /**
+     * Метод для перечисления денег с одного счёта на другой счёт.
+     * Используя паспорт и реквезит находит счет с помощью метода findByRequisite.
+     * Если счета существуют и счет списания больше или равен amount производится перечисление средств.
+     */
 
     public boolean transferMoney(String srcPassport, String srcRequisite,
                                  String destPassport, String destRequisite, double amount) {
