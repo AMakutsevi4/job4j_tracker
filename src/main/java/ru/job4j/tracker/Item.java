3e1d8a4dee3ece4205ffeaab1c320cc0aaeff7fa
AMakutsevi4 <amakutsevich@bk.ru>
2022-09-26 12:07:45 +0900

    1. Lombok. Настройка IDEA

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,11 +1,13 @@
 package ru.job4j.tracker;
 
+import lombok.Data;
+
 import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
-import java.util.Objects;
 
+@Data
 @Entity
 @Table(name = "items")
 public class Item {
@@ -21,12 +23,11 @@ public class Item {
     private LocalDateTime created = LocalDateTime.now().truncatedTo(ChronoUnit.MICROS);
     private String description;
 
-    public Item(int id) {
-        this.id = id;
+    public Item() {
+
     }
 
-    public Item(String name, int id) {
-        this.name = name;
+    public Item(int id) {
         this.id = id;
     }
 
@@ -34,85 +35,14 @@ public class Item {
         this.name = name;
     }
 
+    public Item(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+
     public Item(int id, String name, LocalDateTime created) {
         this.id = id;
         this.name = name;
         this.created = created;
-    }
-
-    public Item(String name, String description, LocalDateTime created) {
-        this.name = name;
-        this.description = description;
-        this.created = created;
-    }
-
-    public Item() {
-
-    }
-
-    @Override
-    public String toString() {
-        return String.format("id: %s, name: %s, created: %s", id, name, FORMATTER.format(created));
-    }
-
-    public int getId() {
-
-        return id;
-    }
-
-    public void setId(int id) {
-
-        this.id = id;
-    }
-
-    public String getName() {
-
-        return name;
-    }
-
-    public void setName(String name) {
-
-        this.name = name;
-    }
-
-    public String getDescription() {
-
-        return description;
-    }
-
-    public void setDescription(String description) {
-
-        this.description = description;
-    }
-
-    public LocalDateTime getCreated() {
-        return created;
-    }
-
-    public void setCreated(LocalDateTime created) {
-        this.created = created;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        Item item = (Item) o;
-        return id == item.id && Objects.equals(name, item.name)
-                && Objects.equals(created.withNano(0), item.created.withNano(0));
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name, created);
-    }
-
-    @Override
-    protected void finalize() throws Throwable {
-        System.out.printf("Removed %d %s%n", id, name);
     }
 }
