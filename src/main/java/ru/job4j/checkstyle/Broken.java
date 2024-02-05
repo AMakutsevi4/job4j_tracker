cc2d88e5946988baf867ecb3c1d0491ea2cfcdbf
AMakutsevi4 <amakutsevich@bk.ru>
2024-02-05 11:41:33 +0900

    6. Правила Checkstyle

---
+++ src/main/java/ru/job4j/checkstyle/Broken.java
@@ -0,0 +1,29 @@
+package ru.job4j.checkstyle;
+
+public class Broken {
+
+    public final String newValue = "";
+    public String surname;
+    public String name;
+    public int sizeOfEmpty = 10;
+
+    Broken() {
+    }
+
+    public void echo() {
+    }
+
+    public void method(int a, int b, int c, int d) {
+
+    }
+
+    public void methodTwo(int e, int f, int g, int h) {
+
+    }
+
+    public void media(Object obj) {
+        if (obj != null) {
+            System.out.println(obj);
+        }
+    }
+}
