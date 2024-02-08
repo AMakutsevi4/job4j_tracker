eb552ad54878dc3f34b306017d9c65d63b300c99
AMakutsevi4 <amakutsevich@bk.ru>
2024-02-08 15:03:59 +0900

    13. Методы преобразования потоков объектов в потоки примитивы и наоборот

---
+++ src/main/java/ru/job4j/stream/mapto/MapToIntExample.java
@@ -0,0 +1,41 @@
+package ru.job4j.stream.mapto;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class MapToIntExample {
+
+    public static void main(String[] args) {
+        List<String> strings = Arrays.asList("1", "2", "3", "4", "5");
+        strings.stream()
+                .mapToInt(Integer::parseInt)
+                .filter(value -> value % 2 == 0)
+                .forEach(System.out::println);
+
+        List<Person> people = Arrays.asList(
+                new Person("Михаил", 35),
+                new Person("Ольга", 26),
+                new Person("Антон", 20),
+                new Person("Виктор", 16),
+                new Person("Анна", 29)
+        );
+
+        int sum = people.stream()
+                .filter(person -> person.getAge() > 25)
+                .mapToInt(Person::getAge)
+                .peek(System.out::println)
+                .sum();
+        System.out.println("Сумма: " + sum);
+
+        int[] array1 = {1, 2, 3};
+        int[] array2 = {4, 5, 6};
+        int[] array3 = {7, 8, 9};
+        List<Integer> list = Stream.of(array1, array2, array3)
+                .flatMapToInt(Arrays::stream)
+                .boxed()
+                .collect(Collectors.toList());
+        System.out.println(list);
+    }
+}
